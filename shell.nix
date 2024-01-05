7f3a2f86a62dc92c2051fb898095bd376b49937e
NanaoPixels <56325750+NanaoPixels@users.noreply.github.com>
2024-01-05 16:00:37 -0300

    Create shell.nix

---
+++ shell.nix
@@ -0,0 +1,26 @@
+let
+  pkgs = import <nixpkgs> {};
+
+in pkgs.python3Packages.buildPythonApplication rec {
+  name = "testing";
+  src = ./.;
+
+  buildInputs = [
+    pkgs.python3Packages.opencv-python
+    pkgs.libGL
+    pkgs.python3Packages.requests
+    pkgs.python3Packages.numpy
+    pkgs.python3Packages.discord.py_2_3_2
+    pkgs.python3Packages.pyproject-toml_0_0_10
+    pkgs.python3Packages.colorama_0_4_6
+    pkgs.python3Packages.moviepy
+    pkgs.python3Packages.huggingface_hub
+    pkgs.python3Packages.unidecode
+    # Add any other required dependencies here
+  ];
+
+  pythonPath = with pkgs.python3Packages; [
+    numpy
+    # Add any other required Python dependencies here
+  ];
+}
